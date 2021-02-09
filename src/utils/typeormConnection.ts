7ff75b3e2a2bb2cbab18bf06a875eb65c48fc62d
Mateus Mello <developer@mindtech.com.br>
2021-02-09 10:41:23 -0300

    --

--- src/utils/typeormConnection.ts
+++ src/utils/typeormConnection.ts
@@ -1,6 +1,6 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
 export async function typeormConnection() {
-	const options = await getConnectionOptions(process.env.NODE_ENV);
+	const options = await getConnectionOptions("default");
 	return createConnection(options);
 }
